f76bc907fb39975fa3d40f0693cbc9b7a9aafb09
sedano <alejandro@sedano.me>
2016-11-25 16:10:54 +0100

    Fix address component returning null + indentation

--- www/js/services/geo.service.js
+++ www/js/services/geo.service.js
@@ -1,40 +1,40 @@
 angular.module('ouium')
-    .service('GeoService', function($q, $rootScope) {
+    .service('GeoService', function ($q, $rootScope) {
 
         var options = { timeout: 10000, enableHighAccuracy: true };
         var geocoder = new google.maps.Geocoder();
 
         function getAddressComponent(address, component, short) {
             var element = null;
-            angular.forEach(address.address_components, function(address_component) {
+            angular.forEach(address.address_components, function (address_component) {
                 if (address_component.types[0] == component) {
                     element = (short) ? address_component.short_name : address_component.long_name;
                 }
             })
-            return element;
+            return element || "";
         }
 
-        var getCurrentPosition = function() {
+        var getCurrentPosition = function () {
             var q = $q.defer();
 
-            navigator.geolocation.getCurrentPosition(function(result) {
+            navigator.geolocation.getCurrentPosition(function (result) {
                 var crd = result.coords;
                 var latlng = new google.maps.LatLng(crd.latitude, crd.longitude);
                 q.resolve(latlng);
-            }, function(err) {
+            }, function (err) {
                 q.reject(err);
             }, options);
 
             return q.promise;
         }
 
-        var geocode = function(query) {
+        var geocode = function (query) {
             var q = $q.defer();
             var result = {};
-            geocoder.geocode(query, function(results, status) {
+            geocoder.geocode(query, function (results, status) {
                 if (status == 'OK') {
                     result.formatted_address = results[0].formatted_address;
-                    result.coordinates = [results[0].geometry.location.lat(),results[0].geometry.location.lng()];
+                    result.coordinates = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
                     result.city = getAddressComponent(results[0], 'locality');
                     result.street = getAddressComponent(results[0], 'route') + " " + getAddressComponent(results[0], 'street_number');
                     result.countryCode = getAddressComponent(results[0], 'country', true);
